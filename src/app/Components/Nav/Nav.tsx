05fb236cb99c5605d69e82e1ed2bdead9b6b9a50
AC9669 <ac9669@student.jamk.fi>
2025-04-08 21:00:40 +0300

    update nav

--- src/app/Components/Nav/Nav.tsx
+++ src/app/Components/Nav/Nav.tsx
@@ -1,21 +1,56 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./Nav.module.css";
 import Link from "next/link";
 
-export default function Nav() {
+type props = {
+  currentLoc: string;
+};
+
+export default function Nav({ currentLoc }: props) {
+  const activeLink: string = styles.linkbox_current;
+
+  useEffect(() => {
+    const home: HTMLElement | null = document.getElementById("linkbox_home");
+    const showcase: HTMLElement | null =
+      document.getElementById("linkbox_showcase");
+    const about: HTMLElement | null = document.getElementById("linkbox_about");
+    const contact: HTMLElement | null =
+      document.getElementById("linkbox_contact");
+
+    switch (currentLoc) {
+      case "home":
+        home?.classList.add(activeLink);
+        break;
+      case "showcase":
+        showcase?.classList.add(activeLink);
+        break;
+      case "about":
+        about?.classList.add(activeLink);
+        break;
+      case "contact":
+        contact?.classList.add(activeLink);
+        break;
+      default:
+        break;
+    }
+  }, [currentLoc, activeLink]);
+
   return (
     <div className={styles.nav}>
-      <div className={styles.linkbox}>
-        <Link href="/">Home</Link>
-      </div>
-      <div className={styles.linkbox}>
-        <Link href="/showcase">Showcase</Link>
-      </div>
-      <div className={styles.linkbox}>
-        <Link href="/about">About Me</Link>
-      </div>
-      <div className={styles.linkbox}>
-        <Link href="/contact">Contact</Link>
+      <div className={styles.links_wrapper}>
+        <div id="linkbox_home" className={styles.linkbox}>
+          <Link href="/">Home</Link>
+        </div>
+        <div id="linkbox_showcase" className={styles.linkbox}>
+          <Link href="/showcase">Showcase</Link>
+        </div>
+        <div id="linkbox_about" className={styles.linkbox}>
+          <Link href="/about">About Me</Link>
+        </div>
+        <div id="linkbox_contact" className={styles.linkbox}>
+          <Link href="/contact">Contact</Link>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./Components/Banner/Banner";
+import LocationBar from "./Components/LocationBar/LocationBar";
 import Nav from "./Components/Nav/Nav";
 
 export default function Home() {
   return (
     <div className="page">
       <Banner />
-      <Nav />
+      <Nav currentLoc="home" />
+      <LocationBar />
     </div>
   );
 }
