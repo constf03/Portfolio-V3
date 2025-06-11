081f1aed70179ad53f92d32d5ee0ddf745fb7b3d
AC9669 <ac9669@student.jamk.fi>
2025-06-11 17:39:28 +0300

    adjust images styles

--- src/app/Components/Slideshow/Slideshow.tsx
+++ src/app/Components/Slideshow/Slideshow.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import styles from "./Slideshow.module.css";
 
 export default function Slideshow() {
   return (
     <Carousel slide={true} interval={4000}>
       <Carousel.Item>
         <img
-          src="https://picsum.photos/seed/a/1600/900"
-          style={{ width: "100%", height: "400px" }}
+          src="/images/display_websites.png"
+          className={styles.slideshow_image}
           alt="..."
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="https://picsum.photos/seed/b/1920/1080"
-          style={{ width: "100%", height: "400px" }}
+          src="/images/display_websites.png"
+          className={styles.slideshow_image}
           alt="..."
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="https://picsum.photos/seed/c/1366/768"
-          style={{ width: "100%", height: "400px" }}
+          src="/images/display_websites.png"
+          className={styles.slideshow_image}
           alt="..."
         />
       </Carousel.Item>
