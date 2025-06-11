af6234997ca08be8416e479231621a6a057f20c1
AC9669 <ac9669@student.jamk.fi>
2025-06-11 17:39:17 +0300

    update marquee banner

--- src/app/Components/MarqueeBanner/MarqueeBanner.tsx
+++ src/app/Components/MarqueeBanner/MarqueeBanner.tsx
@@ -4,7 +4,11 @@ import styles from "./MarqueeBanner.module.css";
 export default function MarqueeBanner() {
   return (
     <div className={styles.banner_container}>
-      <span className={styles.sliding_text}>Welcome To My Portfolio!</span>
+      <span className={styles.sliding_text}>
+        <b>
+          <i>Welcome To My Portfolio!</i>
+        </b>
+      </span>
     </div>
   );
 }
