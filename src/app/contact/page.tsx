292ec02b2704cd74599d743fc505dcd468dffe4f
AC9669 <ac9669@student.jamk.fi>
2025-10-28 16:25:51 +0200

    update styles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,24 +9,31 @@ import Card from "react-bootstrap/Card";
 
 export default function Home() {
   return (
-    <div className="page">
-      <Banner>
-        <Nav currentLoc="contact" />
-      </Banner>
-      <UnderNavBar />
-      <Content>
-        <div style={{ display: "flex" }}>
-          <ContactForm />
-          <Card style={{ maxHeight: "200px" }}>
-            <Card.Body>
-              <Card.Title>Contact Information</Card.Title>
-              <Card.Text>Email: stefano.confalone03@gmail.com</Card.Text>
-              <Card.Text>Phone: +358 45 324 3803</Card.Text>
-            </Card.Body>
-          </Card>
+    <>
+      <div className="page">
+        <div className="page-content">
+          <Banner>
+            <Nav currentLoc="contact" />
+          </Banner>
+          <UnderNavBar />
+          <Content>
+            <div style={{ display: "flex", marginBottom: "100px" }}>
+              <ContactForm />
+              <Card style={{ maxHeight: "200px" }}>
+                <Card.Body>
+                  <Card.Title>Contact Information</Card.Title>
+                  <Card.Text>Email: stefano.confalone03@gmail.com</Card.Text>
+                  <Card.Text>Phone: +358 45 324 3803</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <Footer />
+          </Content>
         </div>
-      </Content>
-      <Footer />
-    </div>
+      </div>
+      <div className="bg-one"></div>
+      <div className="bg-fade"></div>
+      <div className="bg-two"></div>
+    </>
   );
 }
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -8,26 +8,28 @@ type BannerProps = {
 
 export default function Banner({ children }: BannerProps) {
   return (
-    <div className={styles.banner}>
-      <div className={styles.banner_head}>
-        <Image
-          src="/images/con-stefano.jpg"
-          className={styles.portrait}
-          width={128}
-          height={128}
-          alt="..."
-        />
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <h2 className={styles.name_tag}>
-            <b>Stefano Confalone</b>
-          </h2>
-          <span>
-            <i>My Software Development Portfolio</i>
-          </span>
-          <small>Bachelor of Engineering (JAMK 2025)</small>
+    <div className={styles.banner_overlay}>
+      <div className={styles.banner}>
+        <div className={styles.banner_head}>
+          <Image
+            src="/images/con-stefano.jpg"
+            className={styles.portrait}
+            width={128}
+            height={128}
+            alt="..."
+          />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h2 className={styles.name_tag}>
+              <b>Stefano Confalone</b>
+            </h2>
+            <span>
+              <i>My Software Development Portfolio</i>
+            </span>
+            <small>Bachelor of Engineering (JAMK 2025)</small>
+          </div>
         </div>
+        <div className={styles.nav_wrapper}>{children}</div>
       </div>
-      <div className={styles.nav_wrapper}>{children}</div>
     </div>
   );
 }
