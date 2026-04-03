09571ef093f9a4db354e22d208647e3afe4017c6
constf03 <stefano.confalone03@gmail.com>
2026-04-03 17:32:07 +0300

    add fonts utils

---
+++ utils/fonts.ts
@@ -0,0 +1,16 @@
+import { Lato, Saira_Stencil_One, Bebas_Neue } from "next/font/google";
+
+export const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
+
+export const sairaStencilOne = Saira_Stencil_One({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
