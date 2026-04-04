5f13bc352ca1c3b8df56a9cae2022f2bc159d2d0
constf03 <stefano.confalone03@gmail.com>
2026-04-04 16:09:32 +0300

    update contact page and layout

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -3,13 +3,14 @@ import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Contact - Stefano Confalone Sofware Development Portfolio",
-  description: "Software development portfolio contact page.",
+  description:
+    "Want to contact me? Find my contact info and links on this page.",
 };
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return children;
 }
