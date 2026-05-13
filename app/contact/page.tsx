ac1aa38519b7308c9e7f285f268080383f0502c0
constf03 <stefano.confalone03@gmail.com>
2026-05-13 12:00:50 +0300

    hide email from scrapers

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import Banner from "@/components/Banner/Banner";
 import BreadcrumbBar from "@/components/BreadcrumbBar/BreadcrumbBar";
 import Content from "@/components/Content/Content";
@@ -7,6 +8,21 @@ import Nav from "@/components/Nav/Nav";
 import UnderNavBar from "@/components/UnderNavBar/UnderNavBar";
 
 export default function ContactPage() {
+  const EMAIL_USER = ["stefano", "confalone03"].join(".");
+  const EMAIL_DOMAIN = ["gmail", "com"].join(".");
+
+  const PrivacyEmail = () => {
+    const [email, setEmail] = useState<string | null>(null);
+
+    useEffect(() => {
+      setEmail(EMAIL_USER + "@" + EMAIL_DOMAIN);
+    }, []);
+
+    if (!email) return null;
+
+    return <a href={`mailto:${email}`}>{email}</a>;
+  };
+
   return (
     <>
       <div className="page">
@@ -26,25 +42,32 @@ export default function ContactPage() {
                 }}
               >
                 <h2 style={{ marginBottom: "6px" }}>Contact Info & Links</h2>
-                <span>Email: stefano.confalone03@gmail.com</span>
-                <span>Phone: +358 45 324 3803</span>
-
-                <span>
-                  LinkedIn:{" "}
-                  <a
-                    href="https://www.linkedin.com/in/stefano-confalone-a0ba352a7/"
-                    target="blank"
-                  >
-                    https://www.linkedin.com/in/stefano-confalone-a0ba352a7/
-                  </a>
-                </span>
-
-                <span>
-                  GitHub:{" "}
-                  <a href="https://github.com/constf03" target="blank">
-                    https://github.com/constf03
-                  </a>
-                </span>
+                <ul
+                  style={{
+                    listStyle: "none",
+                    marginLeft: "0px",
+                  }}
+                >
+                  <li>
+                    Email: <PrivacyEmail />
+                  </li>
+                  <li>Phone: +358 45 324 3803</li>
+                  <li>
+                    LinkedIn:{" "}
+                    <a
+                      href="https://www.linkedin.com/in/stefano-confalone-a0ba352a7/"
+                      target="blank"
+                    >
+                      https://www.linkedin.com/in/stefano-confalone-a0ba352a7/
+                    </a>
+                  </li>
+                  <li>
+                    GitHub:{" "}
+                    <a href="https://github.com/constf03" target="blank">
+                      https://github.com/constf03
+                    </a>
+                  </li>
+                </ul>
               </div>
             </section>
           </Content>
