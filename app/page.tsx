821091132382bd96c12f6b7ddff4f9eed8dec490
constf03 <stefano.confalone03@gmail.com>
2026-07-01 20:12:17 +0300

    update head pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,89 @@ import Image01 from "@/public/images/krj_website_preview.jpg";
 import Image02 from "@/public/images/offerletterapp_preview.png";
 import MarqueeBanner from "@/components/MarqueeBanner/MarqueeBanner";
 import Nav from "@/components/Nav/Nav";
+import Search from "@/components/Search/Search";
 import UnderNavBar from "@/components/UnderNavBar/UnderNavBar";
-//import Slideshow from "@/components/Slideshow/Slideshow";
+
+function WebSiteList() {
+  const sites = [
+    {
+      name: "Wayback Machine",
+      url: "https://web.archive.org/",
+    },
+    {
+      name: "Frutiger Aero Archive",
+      url: "https://frutigeraeroarchive.org/",
+    },
+    {
+      name: "Wikipedia",
+      url: "https://www.wikipedia.org/",
+    },
+    {
+      name: "Alt Zone",
+      url: "https://altzone.fi/en",
+    },
+    {
+      name: "Johnny Hong Kong",
+      url: "https://ravintolajohnny.fi/",
+    },
+    {
+      name: "Punainen Risti",
+      url: "https://www.punainenristi.fi/",
+    },
+    {
+      name: "ebay.com",
+      url: "https://www.ebay.com/",
+    },
+    {
+      name: "Amazon.com",
+      url: "https://www.amazon.com/",
+    },
+    {
+      name: "Jyväskylän kirjasto",
+      url: "https://www.jyvaskyla.fi/kirjasto",
+    },
+    {
+      name: "Microsoft Learn",
+      url: "https://learn.microsoft.com/en-us/",
+    },
+    {
+      name: "www.newgrounds.com",
+      url: "https://www.newgrounds.com/",
+    },
+    {
+      name: "YouTube",
+      url: "https://www.youtube.com/",
+    },
+    {
+      name: "leetcode.com",
+      url: "https://leetcode.com/",
+    },
+    {
+      name: "DrawIO",
+      url: "https://app.diagrams.net/",
+    },
+    {
+      name: "betsson sport",
+      url: "https://www.betsson.com/fi",
+    },
+  ];
+
+  return (
+    <div className="content-box-wrapper-md">
+      <section className="container-content-box">
+        <div style={{ display: "flex", flexWrap: "wrap", columnGap: 6 }}>
+          {sites.map((site, index) => {
+            return (
+              <a href={site.url} key={index} style={{}}>
+                {site.name}
+              </a>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+}
 
 export default function HomePage() {
   return (
@@ -16,49 +97,67 @@ export default function HomePage() {
       <div className="page">
         <div className="page-content">
           <Banner>
-            <Nav currentLoc="home" />
+            <Nav location="home" />
           </Banner>
           <UnderNavBar />
           <MarqueeBanner />
           <Content>
-            <section style={{ padding: "8px" }} id="content-home">
-              <article className="container-content-box">
-                <h2>Welcome! Tervetuloa! &#128075;</h2>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "end",
+              }}
+            >
+              <Search />
+            </div>
+            <section
+              style={{ marginBottom: "24px", marginTop: "12px" }}
+              id="content-home"
+            >
+              <div className="content-box-wrapper" id="container-welcome">
+                <article className="container-content-box">
+                  <h2>Welcome! Tervetuloa! &#128075;</h2>
 
-                <p>
-                  This is my personal portfolio website I created using Next.js.
-                </p>
+                  <p>
+                    This is my personal portfolio website I created using
+                    Next.js.
+                  </p>
 
-                <p>
-                  I graduated Jyväskylä University of Applied Sciences (Jamk) in
-                  Finland in December 2025 with a Bachelor of Engineering degree
-                  in information and communication technology.
-                </p>
+                  <p>
+                    I graduated Jyväskylä University of Applied Sciences (Jamk)
+                    in Finland in December 2025 with a Bachelor of Engineering
+                    degree in information and communication technology.
+                  </p>
 
-                <p>
-                  On this website you can find showcases of my software
-                  development projects and information about my competences.{" "}
-                  <b>Check out the Projects tab!</b>
-                </p>
-              </article>
-              <aside className="container-content-box">
-                <h3 style={{ marginBottom: "8px" }}>Featured Projects</h3>
+                  <p>
+                    On this website you can find showcases of my software
+                    development projects and information about my competences.{" "}
+                    <b>Check out the Projects tab!</b>
+                  </p>
+                </article>
+              </div>
+              <div className="content-box-wrapper">
+                <aside className="container-content-box">
+                  <h3 style={{ marginBottom: "8px" }}>Featured Projects</h3>
 
-                <FeaturedCard
-                  title="Website For a Restaurant (2024-2025)"
-                  imgSrc={Image01}
-                  imgAltText="Website For a Restaurant Preview"
-                  href="projects/website-for-a-restaurant-(2024-2025)"
-                />
+                  <FeaturedCard
+                    title="Website For a Restaurant (2024-2025)"
+                    imgSrc={Image01}
+                    imgAltText="Website For a Restaurant Preview"
+                    href="projects/website-for-a-restaurant-(2024-2025)"
+                  />
 
-                <FeaturedCard
-                  title="Job Offer Letter Creation App (2025)"
-                  imgSrc={Image02}
-                  imgAltText="Job Offer Letter Creation App Preview"
-                  href="projects/job-offer-letter-creation-app-(2025)"
-                />
-              </aside>
+                  <FeaturedCard
+                    title="Job Offer Letter Creation App (2025)"
+                    imgSrc={Image02}
+                    imgAltText="Job Offer Letter Creation App Preview"
+                    href="projects/job-offer-letter-creation-app-(2025)"
+                  />
+                </aside>
+              </div>
             </section>
+            <WebSiteList />
           </Content>
           <Footer />
         </div>
