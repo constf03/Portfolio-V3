b58454a7f1e20654602a12c2b1e0100bd902ce0b
constf03 <stefano.confalone03@gmail.com>
2026-08-01 18:48:55 +0300

    update banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import { ReactNode } from "react";
 import { bebasNeue } from "@/utils/fonts";
-import Image01 from "@/public/images/con-stefano.jpg";
 import styles from "./Banner.module.css";
 
 interface BannerProps {
@@ -13,27 +11,17 @@ export default function Banner({ children }: BannerProps) {
     <div className={styles.banner_overlay}>
       <div className={styles.banner}>
         <div className={styles.banner_head}>
-          <Image
-            src={Image01}
-            className={styles.portrait}
-            width={128}
-            height={128}
-            alt="Stefano Confalone"
-            loading="lazy"
-          />
           <header style={{ display: "flex", flexDirection: "column" }}>
             <h1 className={`${styles.name_tag} ${bebasNeue.className}`}>
               <strong>Stefano Confalone</strong>
             </h1>
 
-            <span>
-              <i>My Software Development Portfolio</i>
-            </span>
+            <i>Software Developer Portfolio</i>
 
-            <span className={styles.education_tag} style={{ fontSize: "10px" }}>
+            <small className={styles.education_tag} style={{ fontSize: "10px" }}>
               Bachelor of Engineering, Information and Communication Technology
               (Jamk 2025) &#127891;
-            </span>
+            </small>
           </header>
         </div>
 
